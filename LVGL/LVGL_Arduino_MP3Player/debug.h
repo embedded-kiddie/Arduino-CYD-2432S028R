4ee9ff2208635faedb4efe7df5c7ae7c0dedadb1
embedded-kiddie <kings.unlikely@gmail.com>
2025-09-13 19:43:25 +0900

    Rename image files.

--- LVGL/LVGL_Arduino_MP3Player/debug.h
+++ LVGL/LVGL_Arduino_MP3Player/debug.h
@@ -4,15 +4,15 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
-#define DEBUG   1
+#define DEBUG   0
 
-#if   (DEBUG == 2)
+#if   (DEBUG == 1)
 #include <stdio.h>
 #include <assert.h>
 #define DBG_EXEC(x)   x
 #define DBG_ASSERT(x) assert(x)
 
-#elif (DEBUG == 1)
+#elif (DEBUG == 2)
 #include <assert.h>
 #define DBG_EXEC(x)
 #define DBG_ASSERT(x) assert(x)
