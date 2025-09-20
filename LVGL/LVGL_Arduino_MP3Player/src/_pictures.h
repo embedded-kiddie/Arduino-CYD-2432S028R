63857a000113d5a72d828983ddd6ad88c2354611
embedded-kiddie <kings.unlikely@gmail.com>
2025-09-20 13:52:28 +0900

    Update font and icons.

--- LVGL/LVGL_Arduino_MP3Player/src/_pictures.h
+++ LVGL/LVGL_Arduino_MP3Player/src/_pictures.h
@@ -9,6 +9,15 @@ LV_IMAGE_DECLARE(Past_Lives);
 LV_IMAGE_DECLARE(SUPER_EUROBEAT);
 LV_IMAGE_DECLARE(a_nation_vol2);
 LV_IMAGE_DECLARE(Amazing);
+LV_IMAGE_DECLARE(MF_GHOST);
+LV_IMAGE_DECLARE(Avril_Lavigne_Greatest_Hits);
+LV_IMAGE_DECLARE(ballads);
+LV_IMAGE_DECLARE(globe_cruise_record_1995_2000);
+LV_IMAGE_DECLARE(delicious_way);
+LV_IMAGE_DECLARE(DEEP_RIVER);
+LV_IMAGE_DECLARE(HEART_STATION);
+LV_IMAGE_DECLARE(Mrs10);
+LV_IMAGE_DECLARE(Winter_Into_Spring);
 
 const lv_image_dsc_t *pictures[] = {
   /*  0 */  & img_album,
@@ -22,6 +31,13 @@ const lv_image_dsc_t *pictures[] = {
   /*  8 */  & Amazing,
   /*  9 */  & MF_GHOST,
   /* 10 */  & Avril_Lavigne_Greatest_Hits,
+  /* 11 */  & ballads,
+  /* 12 */  & globe_cruise_record_1995_2000,
+  /* 13 */  & delicious_way,
+  /* 14 */  & DEEP_RIVER,
+  /* 15 */  & HEART_STATION,
+  /* 16 */  & Mrs10,
+  /* 17 */  & Winter_Into_Spring,
 };
 
 #define N_PICTURES  (sizeof(pictures) / sizeof(lv_image_dsc_t *))
